059142c38c978db07eb267c8bc0a4c274f7ccd18
Chengyu Mao <mcy02605@gmail.com>
2026-05-31 23:59:23 +0900

    add module framework of engine management system

---
+++ engine_management_system/src/framework/manager.h
@@ -0,0 +1,13 @@
+/**
+ * @file manager.h
+ * @brife Manager class for framework of engine management system
+ * @date 31.5.2026
+ */
+
+ #ifndef MANAGER_H
+ #define MANAGER_H
+
+
+
+
+ #endif
---
+++ engine_management_system/src/framework/module_interface.h
@@ -0,0 +1,16 @@
+/**
+ * @file modul_interface.h
+ * @brief Interface for modules in the engine management system
+ * @date 29.05.2026
+ */
+
+ #ifndef MODULE_INTERFACE_H
+ #define MODULE_INTERFACE_H
+
+ namespace framework {
+ class ModuleInterface {
+ };
+ }
+
+
+ #endif
